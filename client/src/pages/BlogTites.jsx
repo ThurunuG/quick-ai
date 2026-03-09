c4e403f611f943b9d6dc99012cdfea1150fcae27
ThurunuG <thurunugunarathna287@gmail.com>
2026-03-09 15:35:39 +0530

    Complete frontend design

--- client/src/pages/BlogTites.jsx
+++ client/src/pages/BlogTites.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import { Edit, Hash, Sparkles } from 'lucide-react'
+import React, {useState} from 'react'
 
 const BlogTites = () => {
+
+    const blogCategories = ['General', 'Technology', 'Health', 'Travel', 'Food', 'Education', 'Finance', 'Lifestyle']
+  
+    const [selectedCategory, setSelectedCategory] = useState('General')
+    const [input, setInput] = useState('')
+  
+    const onSubmitHandler = async (e) => {
+      e.preventDefault()
+    }
+
   return (
-    <div>BlogTites</div>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#8E37EB]'/>
+          <h1 className='text-xl font-semibold'>AI Title Generator</h1>
+        </div>
+        <p className='mt-6 text-sm font-medium'>Keyword</p>
+        <input onChange={(e)=>setInput(e.target.value)} value={input} type='text' placeholder='The future of artificial intelligence is...' className='w-full mt-2 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#8E37EB]' required/>
+
+        <p className='mt-6 text-sm font-medium'>Category</p>
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {blogCategories.map((item, index) => (
+            <span 
+              onClick={() => setSelectedCategory(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedCategory === item ? 'bg-purple-500 text-white border-purple-500' : 'border-gray-300'}`}
+              key={item}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <br />
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 rounded-lg text-sm cursor-pointer'>
+          <Hash className='w-5' />
+          Generate Title
+        </button>
+      </form>
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
+          <div className='flex items-center gap-3'>
+            <Hash className='w-5 h-5 text-[#8E37EB]'/>
+            <h1 className='text-xl font-semibold'>Generated titles</h1>
+          </div>
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm text-gray-400 flex flex-col items-center gap-5'>
+              <Hash className='w-9 h-9'/>
+              <p>
+                Enter a topic and click "Generate title" to get started
+              </p>
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Eraser, EraserIcon, Sparkles } from 'lucide-react'
+import React, {useState} from 'react'
 
 const RemoveBackground = () => {
+  const [input, setInput] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>Remove Background</h1>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#FF4938]'/>
+          <h1 className='text-xl font-semibold'>Background Removal</h1>
+        </div>
+        <p className='mt-6 text-sm font-medium'>Upload image</p>
+        <input onChange={(e)=>setInput(e.target.files[0])} type='file' accept='image/*' className='w-full mt-2 p-3 border border-gray-300 text-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#8E37EB]' required/>
+
+        <p className='text-xs text-gray-500 font-light mt-1'>Supports JPG, PNG, and other image formats</p>
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 rounded-lg text-sm cursor-pointer'>
+          <Eraser className='w-5' />
+          Remove Background
+        </button>
+      </form>
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
+          <div className='flex items-center gap-3'>
+            <Eraser className='w-5 h-5 text-[#FF4938]'/>
+            <h1 className='text-xl font-semibold'>Processed Image</h1>
+          </div>
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm text-gray-400 flex flex-col items-center gap-5'>
+              <Eraser className='w-9 h-9'/>
+              <p>
+                Upload an image and click "Remove Background" to get started
+              </p>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
